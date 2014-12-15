7187fff9ab382b6f242160b58fae854246c0c481
Daniel-Constantin Mierla <miconda@gmail.com>
2014-12-15 17:25:17 +0100

    initial import of gitpushub code and docs

--- README.md
+++ README.md
@@ -1,4 +1,94 @@
-gitpushub
-=========
+# GITPusHub - PHP Hub Script for Github Push Notifications
 
-PHP script for handling Github PUSH event notifications - send custom email and synchronise local mirror
+## Overview
+
+The script is able to process Github webhook notification for GIT push events.
+
+Features:
+  * verify the secret key to authenticate the notification
+  * do fetch in a local directory that has a mirror of Github repository
+  * send customized email notification
+
+## Installation
+
+Deploy the script and its configuration file to your PHP-enabled web server.
+It was tested with PHP v5.3 and Apache (it includes code for supporting Ngnix).
+
+If the script is available at:
+
+  * https://www.example.com/gitpushub/gitpushub.php
+
+go to the webpage of your project on github.com, then Setting, Webhooks & Services.
+Add a webhook, paste the URL to 'Payload URL' filed and set your secret key. The
+Content-Type has to be 'application/json'. The event for which the hook has to be
+triggered is 'Just the push event.'
+
+Note: if the URL is https and you have an untrusted certificate, be sure to
+disable the check of certificate in the page.
+
+## Configuration:
+
+Sample configuration file: gitpushub-config.php.sample
+
+### Secret Key Verification
+
+Set the same value you added on github.com inside gitpushub-config.php to the
+variable $githubSecret.
+
+### Email Notification
+
+Set the address where to send the notification inside gitpushub-config.php to
+the variable $notifyEmailAddress.
+
+The email format was guided based on Kamailio project custom git commit
+notification. If the number of commits in a push is <=15, then one email is
+sent for each commit. If there are more, then one single email for all commits
+is generated - it includes less details for each commit. The limit to switch
+between one email per commit and combined commits email can be set in config
+via variable $gitCommitsSplitLimit.
+
+For the individual email per commit, if the patch is less than 4096 bytes, then
+it is included in the content of the notification message (easier to review
+directly from email). The size limit can be changed via config variable
+$attachPatchSizeLimit.
+
+
+### GIT Repository Mirroring
+
+First clone your repository with '--mirror' parameter, like:
+
+```
+mkdir -p /tmp/github/mirrors
+cd /tmp/github/mirrors
+git clone --mirror https://github.com/userid/projectid.git projectid
+```
+
+Set the path to the folder with the local GIT repository mirror inside
+gitpushub-config.php to the variable $gitCloneDirectory:
+
+```
+$gitCloneDirectory='/tmp/github/mirrors/projectid';
+```
+
+## Author
+
+Daniel-Constantin Mierla (@miconda)
+Contact: via github
+
+## License
+
+GPLv2
+
+## Contributions
+
+Contributions are welcome, feel free to clone & do pull requests or submit
+improvements & fixes via github tracker.
+
+The initial author activity does not focus on PHP programming, therefore do not
+expect high activity on this project. However, more like a whish list (for the
+spare time or contributors):
+
+  * make the content of email notification based on a templates (one for
+  individual commit notification and one for combined commits notification)
+  * become a hub for all github webhooks notifications (first version was coded
+  and tested only with push notification)
---
+++ gitpushub.php
@@ -0,0 +1,184 @@
+<?php
+
+/**
+ * Script to syncronize local repository mirror and notify upon a commit on github project
+ *    - keep an up to date clone in real time
+ *    - customized email notification (trying to keep same format or better compared with old kamailio.org notifications)
+ * Author: Daniel-Constantin Mierla <miconda@gmail.com>
+ * License: GPLv2
+ */
+
+if(file_exists('gitpushub-config.php')) {
+	include 'gitpushub-config.php';
+}
+
+function send_email_notification($jdoc)
+{
+	global $notifyEmailAddress;
+	global $debugLevel;
+	global $gitCommitsSplitLimit;
+	global $attachPatchSizeLimit;
+
+	$nrcommits = 0;
+
+	foreach ($jdoc->commits as $gcommit) {
+		$nrcommits++;
+	}
+	if($debugLevel>0) error_log('kamailio github notify - number of commits: ' . $nrcommits);
+	if($nrcommits<=0) {
+		return;
+	}
+
+	$mbranch = $jdoc->ref;
+	if (0 === strpos($mbranch, 'refs/heads/')) {
+		$mbranch = substr($mbranch, 11);
+	}
+
+	if($nrcommits<=$gitCommitsSplitLimit) {
+		// one email per commit
+		foreach ($jdoc->commits as $gcommit) {
+			$msbjid = substr($gcommit->id, 0, 8);
+			$mfline = strtok($gcommit->message, "\n");
+			$msubject = "git:" . $mbranch . ":" . $msbjid . ": " . $mfline;
+			$mbody  = "Module: kamailio\n";
+			$mbody .= "Branch: " . $mbranch . "\n";
+			$mbody .= "Commit: " . $gcommit->id . "\n";
+			$mbody .= "URL: " . $gcommit->url . "\n\n";
+			$mbody .= "Author: " . $gcommit->author->name . " <" . $gcommit->author->email . ">\n";
+			$mbody .= "Committer: " . $gcommit->committer->name . " <" . $gcommit->committer->email . ">\n";
+			$mbody .= "Date: " . $gcommit->timestamp . "\n\n";
+			$mbody .= $gcommit->message;
+			$mbody .= "\n\n---\n\n";
+			foreach($gcommit->added as $fcpath) {
+				$mbody .= "Added: " . $fcpath . "\n";
+			}
+			foreach($gcommit->modified as $fcpath) {
+				$mbody .= "Modified: " . $fcpath . "\n";
+			}
+			foreach($gcommit->removed as $fcpath) {
+				$mbody .= "Removed: " . $fcpath . "\n";
+			}
+			$mbody .= "\n---\n\n";
+			$mbody .= "Diff:  " . $gcommit->url . ".diff\n";
+			$mbody .= "Patch: " . $gcommit->url . ".patch\n";
+			$mcdiff = file_get_contents($gcommit->url . ".diff");
+			$mcdifflen = strlen($mcdiff);
+			if($mcdifflen>0 && $mcdifflen<=$attachPatchSizeLimit) {
+				$mbody .= "\n---\n\n";
+				$mbody .= $mcdiff;
+			}
+
+			$mheaders = "X-Mailer: Git\r\nFrom: " . $gcommit->committer->name . " <" . $gcommit->committer->email . ">\r\n";
+			mail($notifyEmailAddress, $msubject, $mbody, $mheaders);
+		}
+		return true;
+	}
+
+	// nrcommits >= $gitCommitsSplitLimit - one email for all commits
+	$msubject = "git: new commits in branch " . $mbranch;
+	$mheaders = "X-Mailer: Git\r\nFrom: " . $jdoc->head_commit->committer->name . " <" . $jdoc->head_commit->committer->email . ">\r\n";
+	foreach ($jdoc->commits as $gcommit) {
+		$mbody .= "- URL:  " . $gcommit->url . "\n";
+		$mbody .= "Author: " . $gcommit->author->name . " <" . $gcommit->author->email . ">\n";
+		$mbody .= "Date:   " . $gcommit->timestamp . "\n\n";
+		$mbody .= $gcommit->message;
+		$mbody .= "\n\n";
+	}
+	mail($notifyEmailAddress, $msubject, $mbody, $mheaders);
+	return true;
+}
+
+/* get all headers function for ngnix */
+if (!function_exists('getallheaders')) 
+{ 
+	function getallheaders() 
+	{
+		$hdrs = '';
+		foreach ($_SERVER as $name => $value)  { 
+			if (substr($name, 0, 5) == 'HTTP_') { 
+				$name = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5))))); 
+				$hdrs[$name] = $value; 
+			} else if ($name == "CONTENT_TYPE") { 
+				$hdrs["Content-Type"] = $value; 
+			} else if ($name == "CONTENT_LENGTH") { 
+				$hdrs["Content-Length"] = $value; 
+			} /* else {
+				$hdrs[$name] = $value; 
+			} */ 
+		} 
+		return $hdrs; 
+	}
+}
+
+/* *** main code part *** */
+
+// github webhooks push notifications - get the headers and the payload
+$headers = getallheaders();
+if(empty($headers)) {
+	if($debugLevel>0) error_log('kamailio github notify - cloning activity: no headers');
+	exit;
+}
+
+$payload = file_get_contents('php://input');
+if(empty($payload)) {
+	if($debugLevel>0) error_log('kamailio github notify - cloning activity: no payload');
+	exit;
+}
+
+if($debugLevel>1) {
+	// write the headers and payload to the file for troubleshooting 
+	if(!empty($notifyLogFile)) {
+		$allheaders = '';
+		foreach ($headers as $name => $value) {
+			$allheaders .= $name . ':' . $value . '\n';
+		}
+		file_put_contents($notifyLogFile, $allheaders . '\n' . $payload . "\n\n-----\n\n", FILE_APPEND | LOCK_EX);
+	}
+
+	// write the payload to the syslog for troubleshooting 
+	if($debugLevel>2) error_log('kamailio github notify - cloning activity: payload [[' . $payload . ']]');
+}
+
+/* checking the secret signature */
+if(!empty($githubSecret)) {
+	// get the header with security signature
+	$hubSignature = $headers['X-Hub-Signature'];
+	if(empty($hubSignature)) {
+		$hubSignature = $headers['x-hub-signature'];
+	}
+
+	if(empty($hubSignature)) {
+		if($debugLevel>0) error_log('kamailio github notify - cloning activity: no signature header');
+		exit;
+	}
+
+	// check if the signature is valid
+	list($algo, $hash) = explode('=', $hubSignature, 2);
+	$payloadHash = hash_hmac($algo, $payload, $githubSecret);
+	if ($hash !== $payloadHash) {
+		if($debugLevel>0) error_log('kamailio github notify - bad notification secret token');
+		exit;
+	}
+}
+
+if(!empty($gitCloneDirectory)) {
+	// fetch remote github to syncronize local clone repository
+	$output = shell_exec( 'cd ' . $gitCloneDirectory . ' && git fetch 2>&1' );
+
+	// write the output of sync command to web server log
+	error_log('kamailio github notify - cloning activity: output [[' . $output . ']]');
+}
+
+if(!empty($notifyEmailAddress)) {
+	// send notification
+	$content = utf8_encode($payload); 
+	$data = json_decode($content);
+
+	if($data) {
+		send_email_notification($data);
+	} else {
+		if($debugLevel>0) error_log('kamailio github notify - no json payload');
+	}
+}
+
+?>
